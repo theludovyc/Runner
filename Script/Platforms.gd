b1df088ad4e5e58dfde95cf0437b14850f9f2f9d
theludovyc <theludovyc@gmail.com>
2018-06-27 23:36:27 +0200

    sidescroll scene

--- Script/Platforms.gd
+++ Script/Platforms.gd
@@ -5,17 +5,30 @@ extends Node
 # var b = "textvar"
 
 export (int) var speed=50
+export (PackedScene) var object=null
+
+func doPop(x, y):
+	var node=object.instance()
+	node.position=Vector2(x, y)
+	add_child(node)
+
+	print(get_child_count())
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+
+	doPop(1600, 0)
+	doPop(3200, 0)
+
 	pass
 
 func _process(delta):
 	for plat in get_children():
-		plat.position+=Vector2(-1, 0)*50*delta
+		plat.position+=Vector2(-1, 0)*speed*delta
 		pass
-	pass
 
-func doPop(object):
-	add_child(object)
+	if get_child(0).position.x<=-1600:
+		doPop(get_child(2).position.x+1600, 0)
+		get_child(0).queue_free()
+	pass
